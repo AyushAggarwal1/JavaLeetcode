538790efc4ba2490366aab747590f775373d36e8
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2023-09-02 10:36:30 +0530

    buy and sell stock 1

---
+++ src/A01Arrays/A02buySellStocks.java
@@ -0,0 +1,25 @@
+package A01Arrays;
+
+public class A02buySellStocks {
+    public static void main(String args[])
+    {
+        int [] prices = {7,1,5,3,6,4};
+        int result = buySell(prices);
+
+        System.out.print(result);
+    }
+
+    public static int buySell(int arr[])
+    {
+        int min_price = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for(int i=0; i<arr.length; i++) 
+        {
+            min_price = Math.min(arr[i],min_price);
+            profit = Math.max(profit, arr[i]-min_price);
+        }
+
+        return profit;
+    }
+}
