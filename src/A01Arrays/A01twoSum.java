50dac8db8dcd600a9b480cf2c520b79462406f2b
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2023-09-02 10:26:35 +0530

    two sum

---
+++ src/A01Arrays/A01twoSum.java
@@ -0,0 +1,28 @@
+package A01Arrays;
+
+public class A01twoSum {
+    public static void main(String args[])
+    {   
+        int arr[] = {2,7,11,15};
+        int target = 9;
+
+        int result[] = twosum(arr, target);
+
+        System.out.print(result[0]+" " +result[1]);
+    }
+
+    public static int[] twosum(int arr[], int target)
+    {
+        for(int i=0; i<arr.length; i++)
+        {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                if(arr[i]+arr[j] == target)
+                {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
