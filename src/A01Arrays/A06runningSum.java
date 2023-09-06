27b9b3f0998bff29890773640f197cb159a8a231
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2023-09-06 22:15:15 +0530

    running sum

---
+++ src/A01Arrays/A06runningSum.java
@@ -0,0 +1,25 @@
+package A01Arrays;
+
+public class A06runningSum 
+{
+    public static int[] runSum(int nums[])
+    {
+        int sum = 0;
+        for(int i=0; i<nums.length; i++)
+        {
+            sum = sum + nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+    }   
+    
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,4};
+        
+        nums = runSum(nums);
+
+        for (int i=0; i<nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
