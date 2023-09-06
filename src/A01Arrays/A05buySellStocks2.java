016e02dfa3ed2d709f4e6fcb03ac9f2817b47853
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2023-09-06 22:05:18 +0530

    buy sell stocks 2

---
+++ src/A01Arrays/A05buySellStocks2.java
@@ -0,0 +1,22 @@
+package A01Arrays;
+public class A05buySellStocks2 
+{
+    public static int maxPro(int nums[])
+    {
+        int profit =0;
+        for(int i=1; i<nums.length; i++)
+        {
+            if(nums[i] > nums[i-1])
+            {
+                profit = profit + (nums[i] - nums[i-1]);
+            }
+        }
+
+        return profit;
+    }
+    public static void main(String[] args) {
+        int nums[] = {7,1,5,3,6,4};
+        int result = maxPro(nums);
+        System.out.println(result);        
+    }   
+}
