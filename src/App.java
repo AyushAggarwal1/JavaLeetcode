43a4e23fa1d2882f17a3a41238e274080594cf3c
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2023-09-14 21:00:25 +0530

    change app.java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      int arr[] = {1,2,43,1,3};
 
-        int n = 7;
-        System.out.println(n/2);
+      for(int i=0; i<arr.length; i++)
+      {
+        min_val =  math.min(arr[i],min_val);
+        maxpro = math.math(maxpro, arr[i]-min_val);
+      }
     }
 }
