c800811820ef5c8bbec8d14ec4aa640b8d905ca2
Ayush Aggarwal <ayushaggarwal1136@gmail.com>
2024-01-01 15:00:39 +0530

    two sum using hashmap

--- src/A01Arrays/A01twoSum.java
+++ src/A01Arrays/A01twoSum.java
@@ -1,6 +1,8 @@
 // https://leetcode.com/problems/two-sum/
 package A01Arrays;
 
+import java.util.*;
+
 public class A01twoSum {
     public static void main(String args[])
     {   
@@ -12,6 +14,7 @@ public class A01twoSum {
         System.out.print(result[0]+" " +result[1]);
     }
 
+    // tc n^2
     public static int[] twosum(int arr[], int target)
     {
         for(int i=0; i<arr.length; i++)
@@ -25,5 +28,26 @@ public class A01twoSum {
             }
         }
         return new int[] {-1,-1};
+    }
+
+    // tc n
+    // find complement -> target - i -> find y -> using hashmap
+
+    public static int[] twosum2(int arr[], int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<arr.length; i++)
+        {
+            int comp = target - arr[i];
+
+            if(map.containsKey(comp))
+            {
+                return new int[]{map.get(comp), i};
+            }
+
+            map.put(arr[i], i);
+        }
+
+        return new int[-1];
     }
 }
